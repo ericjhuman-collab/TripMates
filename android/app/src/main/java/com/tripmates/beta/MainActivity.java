70c06afb07d0a697eea7cb010f776adbad4ae36c
Forgotname92 <charlie.nilsson@live.com>
2026-04-28 20:51:22 +0200

    Add Capacitor for iOS + Android beta builds

---
+++ android/app/src/main/java/com/tripmates/beta/MainActivity.java
@@ -0,0 +1,5 @@
+package com.tripmates.beta;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tripmates.beta',
+  appName: 'TripMates',
+  webDir: 'dist'
+};
+
+export default config;
